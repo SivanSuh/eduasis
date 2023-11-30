793af81a4227d7f680e2f04438fa2b676c0702b7
sivansuh <sivansuh.is@gmail.com>
2023-11-30 14:50:20 +0300

    modal setup

---
+++ src/component/Popup/index.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from "react";
+import PopupProps from "./props";
+import Style from "./style.module.css";
+
+const Popup: FC<PopupProps> = ({ children, close, open = false }) => {
+  return (
+    <>
+      {open && (
+        <div className={Style.popup}>
+          {/* <p onClick={() => close?.(false)}>X</p> */}
+          <main>{children}</main>
+        </div>
+      )}
+    </>
+  );
+};
+export default Popup;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,29 @@
 import Avatar from "@/component/Avatar";
-import { ButtonGroup } from "@/mock/index";
 import Button from "@/component/Button";
+import { useState } from "react";
+import Popup from "@/component/Popup";
 
 export default function Home() {
+  const [select, setSelect] = useState<string | undefined>("");
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleMouseUp = () => {
+    console.log(
+      "window.getSelection().anchorNode.data => ",
+      window.getSelection()?.toString()
+    );
+    setSelect(window.getSelection()?.toString());
+    setOpen(true);
+  };
+
   return (
     <main className={`flex  flex-col items-start p-24 `}>
       <figure className="flex items-center gap-4 my-3">
         <Avatar image="https://eduasis.io/eduasis-logo-white-text.svg" />
         <strong>Reading</strong>
       </figure>
-      <div className="flex gap-3 items-center my-2">
-        {ButtonGroup.map((item) => (
-          <Button title={item.buttonTitle} key={item.index} />
-        ))}
-      </div>
-      <p>
+
+      <p onMouseUp={handleMouseUp}>
         The App Router works in a new directory named app. The app directory
         works alongside the pages directory to allow for incremental adoption.
         This allows you to opt some routes of your application into the new
@@ -22,6 +31,15 @@ export default function Home() {
         behavior. If your application uses the pages directory, please also see
         the Pages Router documentation.
       </p>
+      {select && (
+        <Popup open={open}>
+          <div className="flex flex-col items-center gap-3 p-2">
+            {select}
+            <Button title="Add To FlashCar" />
+            <Button title="Explain in Context" />
+          </div>
+        </Popup>
+      )}
     </main>
   );
 }
